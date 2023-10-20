bee4566a98e382aa365445b3c1423ce11664e160
Desiana <desianadf01@gmail.com>
2023-10-20 10:37:04 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('Hallo kita akan membuat RESTful API');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    code: '200',
+    status: 'OK',
+    message: 'Welcome to the Pramasti API'});
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send({
+    code: '404',
+    status: 'Not Found',
+    errors: {
+      message: 'The page or resource you\'re looking for could not be found.',
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Pramasti web listening at http://localhost:${port}`);
+});
